93f490cc5ba0dfd18913e9353a4f23afbb9c3be9
edourde FERRARI <abdirrazzak44@gmail.co>
2025-09-04 23:09:41 +0200

    Test sur qrcode

--- back/src/main/java/com/example/jeux_olimpique/service/ReservationService.java
+++ back/src/main/java/com/example/jeux_olimpique/service/ReservationService.java
@@ -51,7 +51,7 @@ public class ReservationService {
 
 	    String accountKey = user.getAccountKey();
 	    String ticketKey = utilss.generateKey();
-	    String finalKey = accountKey + ":" + ticketKey;
+	    String finalKey = accountKey + "et" + ticketKey;
 	    String validateUrl = baseUrl + "?finalKey=" + finalKey;
 	    String qrCode = utilss.generateQRCode(validateUrl);
 	    Reservation reservation = new Reservation();
